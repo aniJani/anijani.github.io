0b44b438a225023fd1b56635dee3ccd24ca39415
AniJani <rajkarnikarjanit@gmail.com>
2024-08-23 15:35:17 -0500

    done nearly

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,12 +1,25 @@
+"use client";
 import React from 'react';
 
 const Navbar = () => {
+    const scrollToProjects = (e) => {
+        e.preventDefault();
+        const projectsSection = document.getElementById('projects-section');
+        if (projectsSection) {
+            projectsSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
-        <div className="flex justify-start items-start  bg-black">
+        <div className="flex justify-start items-start bg-black">
             <div className="flex flex-row border-2 border-white p-4 bg-black">
-                <div className="text-white font-bold text-l uppercase tracking-wider cursor-pointer hover:bg-gray-800 px-4 py-2">Home</div>
-                <div className="text-white font-bold text-l uppercase tracking-wider cursor-pointer hover:bg-gray-800 px-4 py-2 ">Projects</div>
-                <div className="text-white font-bold text-l uppercase tracking-wider cursor-pointer hover:bg-gray-800 px-4 py-2 ">CV</div>
+                <div
+                    onClick={scrollToProjects}
+                    className="text-white font-bold text-l uppercase tracking-wider cursor-pointer hover:bg-gray-800 px-4 py-2"
+                >
+                    Projects
+                </div>
+                <div className="text-white font-bold text-l uppercase tracking-wider cursor-pointer hover:bg-gray-800 px-4 py-2">CV</div>
             </div>
         </div>
     );
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
+"use client"
 import { Github, Linkedin } from 'lucide-react';
 import Image from 'next/image';
+import { useEffect } from 'react';
 import Navbar from "./components/navbar";
 import { Card, Carousel } from "./components/ui/apple-cards-carousel";
 
@@ -31,94 +33,122 @@ export default function Home() {
     { category: "", title: "", src: "/Emoki (6).jpg" },
   ];
 
+  useEffect(() => {
+    const handleClick = (e) => {
+      e.preventDefault();
+      const targetId = e.target.getAttribute('href').slice(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    };
+
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(link => {
+      link.addEventListener('click', handleClick);
+    });
+
+    return () => {
+      links.forEach(link => {
+        link.removeEventListener('click', handleClick);
+      });
+    };
+  }, []);
+
   return (
-    <main className="min-h-screen bg-black flex flex-col items-center p-4 w-full sm:p-14">
-      <div className="w-full max-w-4xl flex flex-col md:flex-row justify-center items-center">
-        <div className="w-full md:w-auto mb-10 md:mb-0 md:mr-10 text-center">
-          <div className="flex justify-center mb-8 md:ml-[-30px]">
-            <Navbar />
+    <>
+      <style jsx global>{`
+      .smooth-scroll {
+        scroll-behavior: smooth;
+      }
+    `}</style>
+      <main className="min-h-screen bg-black flex flex-col items-center p-4 w-full sm:p-14 smooth-scroll">
+        <div className="w-full max-w-4xl flex flex-col md:flex-row justify-center items-center">
+          <div className="w-full md:w-auto mb-10 md:mb-0 md:mr-10 text-center">
+            <div className="flex justify-center mb-8 md:ml-[-30px]">
+              <Navbar />
+            </div>
+            <div className="mt-8">
+              <Image
+                src="/Headshot1.png"
+                alt="Descriptive alt text"
+                width={290}
+                height={230}
+                className="mx-auto object-contain rounded-lg shadow-lg"
+              />
+              <div className="mt-4 space-y-2">
+                <a href="https://www.linkedin.com/in/janit-rajkarnikar-18b2782b1/" className="flex items-center justify-center text-gray-300 hover:text-white">
+                  <Linkedin className="w-5 h-5 mr-2" />
+                  LinkedIn
+                </a>
+                <a href="https://github.com/aniJani" className="flex items-center justify-center text-gray-300 hover:text-white">
+                  <Github className="w-5 h-5 mr-2" />
+                  Github
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="mt-8">
-            <Image
-              src="/Headshot1.png"
-              alt="Descriptive alt text"
-              width={290}
-              height={230}
-              className="mx-auto object-contain rounded-lg shadow-lg"
-            />
-            <div className="mt-4 space-y-2">
-              <a href="https://www.linkedin.com/in/janit-rajkarnikar-18b2782b1/" className="flex items-center justify-center text-gray-300 hover:text-white">
-                <Linkedin className="w-5 h-5 mr-2" />
-                LinkedIn
-              </a>
-              <a href="https://github.com/aniJani" className="flex items-center justify-center text-gray-300 hover:text-white">
-                <Github className="w-5 h-5 mr-2" />
-                Github
-              </a>
+
+          <div className="w-full md:w-auto text-center md:text-left md:ml-10 md:mt-20">
+            <div className="mt-8 text-white max-w-xl">
+              <h1 className="text-5xl font-bold">Hi, I'm Janit.</h1>
+              <p className="mt-4 text-lg">
+                I'm in my second year of undergrad, majoring in <span className="font-semibold text-white">Computer Science</span> with a minor in <span className="font-semibold text-white">Economic Data Analysis</span>.
+                I have hands-on experience with Machine Learning techniques and enjoy working on applicaitons that leverage data-driven insights to solve real-world problems.
+              </p>
+              <p className="mt-4 text-lg">
+                I'm also interested in research, particularly in cybersecurity, where I'm delving into key challenges and contributing to the development of practical solutions.
+              </p>
             </div>
           </div>
         </div>
 
-        <div className="w-full md:w-auto text-center md:text-left md:ml-10 md:mt-20">
-          <div className="mt-8 text-white max-w-xl">
-            <h1 className="text-5xl font-bold">Hi, I'm Janit.</h1>
-            <p className="mt-4 text-lg">
-              I'm in my second year of undergrad, majoring in <span className="font-semibold text-white">Computer Science</span> with a minor in <span className="font-semibold text-white">Economic Data Analysis</span>.
-              I am broadly interested in leveraging computational methods to develop tools and simulations that accelerate advancements in technology.
-            </p>
-            <p className="mt-4 text-lg">
-              My background includes hands-on research experience in software engineering, data analysis, and machine learning.
-              I'm passionate about creating innovative solutions to complex problems.
-            </p>
+        <section id="projects-section" className="w-full max-w-5xl mt-20">
+          <div className="flex items-start mb-12">
+            <h2 className="text-4xl font-bold text-white text-left mr-8">Projects</h2>
+            <div className="text-gray-400 text-sm">
+              <ul className="space-y-2">
+                <li>
+                  <a href="#calorie-tracker" className="hover:underline transition-all duration-300 ease-in-out">1. Calorie Tracker</a>
+                </li>
+                <li>
+                  <a href="#designers-touch" className="hover:underline transition-all duration-300 ease-in-out">2. The Designer's Touch</a>
+                </li>
+                <li>
+                  <a href="#emoki" className="hover:underline transition-all duration-300 ease-in-out">3. Emoki</a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </div>
 
-      <section className="w-full max-w-5xl mt-20">
-        <div className="flex items-start mb-12">
-          <h2 className="text-4xl font-bold text-white text-left mr-8">Projects</h2>
-          <div className="text-gray-400 text-sm">
-            <ul className="space-y-2">
-              <li>
-                <a href="#calorie-tracker" className="hover:underline transition-all duration-300 ease-in-out">1. Calorie Tracker</a>
-              </li>
-              <li>
-                <a href="#designers-touch" className="hover:underline transition-all duration-300 ease-in-out">2. The Designer's Touch</a>
-              </li>
-              <li>
-                <a href="#emoki" className="hover:underline transition-all duration-300 ease-in-out">3. Emoki</a>
-              </li>
-            </ul>
+          {/* Calorie Tracker */}
+          <div id="calorie-tracker" className="mb-12 scroll-mt-20">
+            <h3 className="text-2xl font-semibold text-white mb-2">Calorie Tracker</h3>
+            <p className="text-gray-300 mb-2">A mobile app with a very simple interface that helps users track their daily calorie intake.</p>
+            <Carousel items={carouselItems.map((item, index) => (
+              <Card key={index} card={item} index={index} layout={true} />
+            ))} />
           </div>
-        </div>
 
-        {/* Calorie Tracker */}
-        <div id="calorie-tracker" className="mb-12 scroll-mt-20">
-          <h3 className="text-2xl font-semibold text-white mb-2">Calorie Tracker</h3>
-          <p className="text-gray-300 mb-2">A mobile app with a very simple interface that helps users track their daily calorie intake.</p>
-          <Carousel items={carouselItems.map((item, index) => (
-            <Card key={index} card={item} index={index} layout={true} />
-          ))} />
-        </div>
+          {/* The Designer's Touch */}
+          <div id="designers-touch" className="mb-12 scroll-mt-20">
+            <h3 className="text-2xl font-semibold text-white mb-2">The Designer's Touch</h3>
+            <p className="text-gray-300">A creative platform where users can design custom t-shirts and other merchandise, with a real-time 3D preview.</p>
+            <Carousel items={carouselItems1.map((item, index) => (
+              <Card key={index} card={item} index={index} layout={true} />
+            ))} />
+          </div>
 
-        {/* The Designer's Touch */}
-        <div id="designers-touch" className="mb-12 scroll-mt-20">
-          <h3 className="text-2xl font-semibold text-white mb-2">The Designer's Touch</h3>
-          <p className="text-gray-300">A creative platform where users can design custom t-shirts and other merchandise, with a real-time 3D preview.</p>
-          <Carousel items={carouselItems1.map((item, index) => (
-            <Card key={index} card={item} index={index} layout={true} />
-          ))} />
-        </div>
-
-        {/* Emoki */}
-        <div id="emoki" className="mb-12 scroll-mt-20">
-          <h3 className="text-2xl font-semibold text-white mb-2">Emoki</h3>
-          <p className="text-gray-300 mb-2">An emotion tracking app that aggregates users' emotions over time, helping them understand their emotional patterns.</p>
-          <Carousel items={carouselItems2.map((item, index) => (
-            <Card key={index} card={item} index={index} layout={true} />
-          ))} />
-        </div>
-      </section>
-    </main>
+          {/* Emoki */}
+          <div id="emoki" className="mb-12 scroll-mt-20">
+            <h3 className="text-2xl font-semibold text-white mb-2">Emoki</h3>
+            <p className="text-gray-300 mb-2">An emotion tracking app that aggregates users' emotions over time, helping them understand their emotional patterns.</p>
+            <Carousel items={carouselItems2.map((item, index) => (
+              <Card key={index} card={item} index={index} layout={true} />
+            ))} />
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
