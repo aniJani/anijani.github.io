96a4f53e031d2725b1a1fb178d4831d82539f907
AniJani <rajkarnikarjanit@gmail.com>
2024-08-23 20:04:02 -0500

    attempt

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/rajkarnikar_portfolio',
+    assetPrefix: '/rajkarnikar_portfolio/',
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
