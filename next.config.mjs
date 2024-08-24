7e7c1f4e46d891c1084170fc816184ff2d19ae90
AniJani <rajkarnikarjanit@gmail.com>
2024-08-23 19:34:29 -0500

    retry

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/rajkarnikar_portfolio",
-    assetPrefix: "/rajkarnikar_portfolio",
     output: 'export',
 };
+
 export default nextConfig;
