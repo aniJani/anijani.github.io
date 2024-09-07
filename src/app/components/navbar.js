87c54db4beae2bb6071651433d98438f727454e8
AniJani <rajkarnikarjanit@gmail.com>
2024-09-06 23:25:48 -0500

    resume in new tab

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,4 +1,5 @@
 "use client";
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
@@ -13,23 +14,25 @@ const Navbar = () => {
         }
     };
 
-    const navigateToResume = () => {
-        router.push('../../CV');
-    };
-
     return (
-        <div className="flex justify-start items-start bg-black">
+        <nav className="flex justify-start items-start bg-black">
             <div className="flex flex-row border-2 border-white p-4 bg-black">
-                <div
+                <button
                     onClick={scrollToProjects}
                     className="text-white font-bold text-l uppercase tracking-wider cursor-pointer hover:bg-gray-800 px-4 py-2"
                 >
                     Projects
-                </div>
-                <div onClick={navigateToResume}
-                    className="text-white font-bold text-l uppercase tracking-wider cursor-pointer hover:bg-gray-800 px-4 py-2">CV</div>
+                </button>
+                <Link
+                    href="/resume.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white font-bold text-l uppercase tracking-wider cursor-pointer hover:bg-gray-800 px-4 py-2"
+                >
+                    CV
+                </Link>
             </div>
-        </div>
+        </nav>
     );
 };
 
