ae1dfe0ce4d5c8cd43d40bfc0cce25d713a8bd5e
AniJani <rajkarnikarjanit@gmail.com>
2025-01-03 12:21:19 -0600

    Resume added

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
                     Projects
                 </button>
                 <Link
-                    href="/Resume.pdf"
+                    href="/Rajkarnikar Janit.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-white font-bold text-l uppercase tracking-wider cursor-pointer hover:bg-gray-800 px-4 py-2"
